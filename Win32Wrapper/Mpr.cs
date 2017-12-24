61124c4846611f4bccbcdda74401c339b311891f
yakumo <yakumo.yakumo.yakumo@gmail.com>
2017-12-25 03:40:37 +0900

    WNetGetUniversalNameが呼び出せる

---
+++ Win32Wrapper/Mpr.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Yac.Win32Wrapper.Raw
+{ 
+    public class Mpr
+    {
+        public struct UNIVERSAL_NAME_INFO
+        {
+            public string lpUniversalName;
+        }
+        
+        public struct REMOTE_NAME_INFO
+        {
+            public string lpUniversalName;
+            public string lpConnectionName;
+            public string lpRemainingPath;
+        }
+
+        public struct INFO_LEVEL
+        {
+            public const int UNIVERSAL_NAME_INFO_LEVEL = 0x00000001;
+            public const int REMOTE_NAME_INFO_LEVEL = 0x00000002;
+        }
+
+        [DllImport("mpr.dll", CharSet = CharSet.Unicode)]
+        [return:MarshalAs(UnmanagedType.U4)]
+        public static extern int WNetGetUniversalName(
+        string lpLocalPath,
+        [MarshalAs(UnmanagedType.U4)] int dwInfoLevel,
+        IntPtr lpBuffer,
+        [MarshalAs(UnmanagedType.U4)] ref int lpBufferSize);
+    }
+}
--- Win32WrapperTest/Win32WrapperTest.cs
+++ Win32WrapperTest/Win32WrapperTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Yac.Win32Wrapper;
 using Yac.Win32Wrapper.Raw;
+using System.Runtime.InteropServices;
 
 namespace Win32WrapperTest
 {
@@ -12,9 +13,28 @@ namespace Win32WrapperTest
             DebugMessage.Info("Kernel32Test");
         }
 
+        private static void MarpTest()
+        {
+            string path = Environment.GetCommandLineArgs()[0];
+
+            IntPtr lpBuffer = IntPtr.Zero;
+            int size = 0;
+            int errorCode = Mpr.WNetGetUniversalName(path, Mpr.INFO_LEVEL.UNIVERSAL_NAME_INFO_LEVEL, lpBuffer, ref size);
+            if (errorCode != 0)
+            {
+                DebugMessage.Err("errorCode = " + errorCode);
+                return;
+            }
+            Mpr.UNIVERSAL_NAME_INFO info = new Mpr.UNIVERSAL_NAME_INFO();
+            Marshal.PtrToStructure(lpBuffer, info);
+            DebugMessage.Info("unc path = " + info.lpUniversalName);
+            
+        }
+
         static void Main(string[] args)
         {
             Kernel32Test();
+            MarpTest();
         }
     }
 }
